759b2029d7e6a3cf7669b57e906bc7871290fd0c
Rafael Francisco <rafafrancisco03@gmail.com>
2026-06-27 21:21:03 -0300

    Refactor number input and comparison logic

--- MaiorMenor1.cs
+++ MaiorMenor1.cs
@@ -1,27 +1,28 @@
-﻿// Projeto criado apenas para fins academicos.
-
-// Projeto MaiorMenor  
-
-
-
 using System;
 
 public class Program
+
 {
     public static void Main(string[] args)
-    {
-        Console.WriteLine("Digite o primeiro número:");
-        int num1 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite o segundo número:");
-        int num2 = int.Parse(Console.ReadLine());
+    {
+        int num1, num2;
+        Console.WriteLine("Digite o primeiro número: ");
+       
+        num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo número: ");
+       
+        num2 = int.Parse(Console.ReadLine());
 
         if (num1 > num2)
         {
-            Console.WriteLine($"O maior número é: {num1}");
+            Console.WriteLine("O maior número é: " + num1);
         }
-        
-        else if (num1 == num2)
+        else if (num2 > num1)
+        {
+            Console.WriteLine("O maior número é: " + num2);
+        }
+        else
         {
             Console.WriteLine("Os números são iguais.");
         }
